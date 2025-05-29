a72de561215c3db7f383f3d7f78eed831dff21f1
jsuryahyd <jaysuryahyd@gmail.com>
2025-05-29 16:54:27 +0530

    Added logger with context

--- backend-challenge/internal/app/server.go
+++ backend-challenge/internal/app/server.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jsuryahyd/food-cart-order-service/internal/common/config"
+	"github.com/jsuryahyd/food-cart-order-service/internal/common/logging"
 )
 
 func RunServer(ctx context.Context, config *config.Config) error {
@@ -19,12 +20,15 @@ func RunServer(ctx context.Context, config *config.Config) error {
 
 	//todo: initiate redis
 
-	router := gin.Default()
+	router := gin.New()
+
+	logging.InitLogger()
+	router.Use(logging.GinLogger())
+	router.Use(gin.Recovery())
+
 	router.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
 	})
-
-	//static
 	//scalar api client
 	router.Static("/scalar", "./web/scalar")
 	router.StaticFile("/openapi.yaml", "./api/openapi.yaml")
@@ -38,21 +42,20 @@ func RunServer(ctx context.Context, config *config.Config) error {
 		WriteTimeout: config.Server.WriteTimeout,
 		IdleTimeout:  config.Server.IdleTimeout,
 	}
-
+	logger := logging.GetLogger()
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
-			//todo: use logger
-			fmt.Printf("server failed to start and listen %w", err) //todo: improper formatting in terminal, after server is shutdown
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Errorw("server failed to start and listen", "error", err)
 		}
 	}()
-	fmt.Printf("server info %v", server)
+
 	//graceful shutdown
-	exit := make(chan os.Signal)
+	exit := make(chan os.Signal, 1)
 
 	signal.Notify(exit, syscall.SIGINT /*CTRL+C*/, syscall.SIGTERM /*Docker stop signals*/)
 	<-exit //block until signal is received
 
-	fmt.Println("Stop signal received, shutting down the server...")
+	logger.Info("Stop signal received, shutting down the server...")
 
 	ctxWithTimeout, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
@@ -60,7 +63,7 @@ func RunServer(ctx context.Context, config *config.Config) error {
 	if err := server.Shutdown(ctxWithTimeout); err != nil {
 		return fmt.Errorf("Server force shutdown")
 	}
-	fmt.Println("Server shutdown gracefully")
+	logger.Info("Server shutdown gracefully")
 
 	return nil
 }
